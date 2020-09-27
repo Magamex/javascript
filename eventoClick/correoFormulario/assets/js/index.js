c83f5a279973db7fbefb9596f479829f3da9dc30
Magamex <matias.angeluk@gmail.com>
2020-09-26 22:36:48 -0300

    +simple ejemplo sobre eventos - simula envio del correo

---
+++ eventoClick/correoFormulario/assets/js/index.js
@@ -0,0 +1,42 @@
+document.querySelector('#botonEnviar').addEventListener('click', function(evento){
+    evento.preventDefault();
+    enviandoCorreo();
+})
+
+document.querySelector('#botonLimpiar').addEventListener('click',function(evento){
+    evento.preventDefault();
+    limpiarInput();
+})
+
+function limpiarInput(){
+    var inputCorreo = document.getElementById('inputCorreo');
+    var inputAsunto = document.getElementById('inputAsunto');
+    var inputCuerpo = document.getElementById('inputCuerpo');
+
+    inputCorreo.value='';
+    inputAsunto.value='';
+    inputCuerpo.value='';
+}
+
+function enviandoCorreo(){
+    var divRespuesta = document.getElementById('mensajeRespuesta');
+    divRespuesta.removeAttribute('hidden');
+    var mensajeRespuesta = document.createElement('img');
+    mensajeRespuesta.id = 'imagenRespuesta';
+    mensajeRespuesta.style.height = "150px"
+    mensajeRespuesta.src = './assets/image/spinner.gif';
+    document.getElementById('mensajeRespuesta').appendChild(mensajeRespuesta);
+    setTimeout(cambiarEnviando, 4000);
+}
+
+function cambiarEnviando(){
+    var tagImagen = document.getElementById('imagenRespuesta');
+    tagImagen.src = './assets/image/mailSend.gif';
+    setTimeout(borrarImg, 2000);
+}
+
+function borrarImg(){
+    var divRespuesta = document.getElementById('imagenRespuesta');
+    divRespuesta.remove();
+    limpiarInput()
+}
